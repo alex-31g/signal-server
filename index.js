6f37b536adea8c669bd5a6529efa97c214c1e899
alex-31g <al.tsimbalyuk@gmail.com>
2023-05-27 02:29:41 +0300

    'Init'

---
+++ index.js
@@ -0,0 +1,31 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var http_1 = require("http");
+var socket_io_1 = require("socket.io");
+// Create an Express app
+var app = (0, express_1.default)();
+var server = (0, http_1.createServer)(app);
+// Create a Socket.IO server with CORS enabled for all hosts
+var io = new socket_io_1.Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+    },
+});
+io.on("connection", function (socket) {
+    console.log("connected");
+    socket.on("offer", function (data) { return socket.broadcast.emit("offer", data); });
+    socket.on("answer", function (data) { return socket.broadcast.emit("answer", data); });
+    socket.on("iceCandidate", function (data) {
+        return socket.broadcast.emit("iceCandidate", data);
+    });
+    socket.on("disconnect", function () { return console.log("disconnected"); });
+});
+var port = process.env.PORT || 3000;
+server.listen(port, function () {
+    console.log("Server listening on port ".concat(port));
+});
---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import { createServer } from "http";
+import { Server, Socket } from "socket.io";
+
+// Create an Express app
+const app = express();
+const server = createServer(app);
+
+// Create a Socket.IO server with CORS enabled for all hosts
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket: Socket) => {
+  console.log("connected");
+
+  socket.on("offer", (data) => socket.broadcast.emit("offer", data));
+
+  socket.on("answer", (data) => socket.broadcast.emit("answer", data));
+
+  socket.on("iceCandidate", (data) =>
+    socket.broadcast.emit("iceCandidate", data)
+  );
+
+  socket.on("disconnect", () => console.log("disconnected"));
+});
+
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
